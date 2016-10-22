3a3f913e63e4356fd7e43c8c7a26073424a50306
karoldavid <k.zysk@zoho.com>
2016-10-22 17:36:26 +0200

    add google maps map and get initial location data via google maps places service testSearch request

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -1,4 +1,40 @@
-'use strict';
+"use strict";
 
-console.log('\'Allo \'Allo!');
+var map;
+
+var locations = [];
+
+function initMap() {
+
+	console.log("initMap");
+
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: { lat: 52.516258, lng: 13.377697 }, // Berlin
+		zoom: 12,
+		//styles: styles,
+		mapTypeControl: false
+	});
+
+	function textSearchPlaces() {
+		var bounds = map.getBounds();
+		var placesService = new google.maps.places.PlacesService(map);
+
+		placesService.textSearch({
+			query: "restaurants",
+			bounds: bounds
+		}, function (results, status) {
+			if (status === google.maps.places.PlacesServiceStatus.OK) {
+				console.log(results);
+			}
+		});
+	}
+
+	textSearchPlaces();
+}
+
+var ViewModel = function ViewModel() {
+	var self = this;
+};
+
+ko.applyBindings(new ViewModel());
 //# sourceMappingURL=main.js.map
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,43 @@
-console.log('\'Allo \'Allo!');
+var map;
+
+var locations = [
+
+];
+
+
+function initMap() {
+
+	console.log("initMap");
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 52.516258, lng: 13.377697}, // Berlin
+        zoom: 12,
+        //styles: styles,
+        mapTypeControl: false
+    });
+
+	function textSearchPlaces() {
+	    var bounds = map.getBounds();
+	    var placesService = new google.maps.places.PlacesService(map);
+	    
+	    placesService.textSearch({
+	        query: "restaurants",
+	        bounds: bounds
+	    }, function(results, status) {
+	    	if (status === google.maps.places.PlacesServiceStatus.OK) {
+	        	console.log(results);
+	        }
+	    });
+	}
+
+	textSearchPlaces();
+
+}
+
+var ViewModel = function () {
+	var self = this;
+
+};
+
+
+ko.applyBindings(new ViewModel());
