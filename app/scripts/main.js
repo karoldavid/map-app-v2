4e1e0260a474b4391cec29eeaf85ed568d7bd8b0
karoldavid <k.zysk@zoho.com>
2017-01-21 18:37:31 +0100

    feat: set map center to user geolocation and perform nearBy search

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,209 +1,208 @@
-var map,
-    vm;
+var map;
 
 /** @credit: https://snazzymaps.com/style/80276/causely-map */
 var styles = [{
-    "featureType": "administrative",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-        "color": "#333333"
+    'featureType': 'administrative',
+    'elementType': 'labels.text.fill',
+    'stylers': [{
+        'color': '#333333'
     }, {
-        "lightness": "40"
+        'lightness': '40'
     }]
 }, {
-    "featureType": "administrative.country",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-        "color": "#007e7a"
+    'featureType': 'administrative.country',
+    'elementType': 'labels.text.fill',
+    'stylers': [{
+        'color': '#007e7a'
     }]
 }, {
-    "featureType": "administrative.province",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-        "color": "#333333"
+    'featureType': 'administrative.province',
+    'elementType': 'labels.text.fill',
+    'stylers': [{
+        'color': '#333333'
     }]
 }, {
-    "featureType": "administrative.locality",
-    "elementType": "labels.text",
-    "stylers": [{
-        "weight": "1.42"
+    'featureType': 'administrative.locality',
+    'elementType': 'labels.text',
+    'stylers': [{
+        'weight': '1.42'
     }]
 }, {
-    "featureType": "administrative.neighborhood",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-        "color": "#007e7a"
+    'featureType': 'administrative.neighborhood',
+    'elementType': 'labels.text.fill',
+    'stylers': [{
+        'color': '#007e7a'
     }, {
-        "lightness": "15"
+        'lightness': '15'
     }]
 }, {
-    "featureType": "landscape.man_made",
-    "elementType": "geometry.fill",
-    "stylers": [{
-        "color": "#00b1ac"
+    'featureType': 'landscape.man_made',
+    'elementType': 'geometry.fill',
+    'stylers': [{
+        'color': '#00b1ac'
     }, {
-        "lightness": "75"
+        'lightness': '75'
     }, {
-        "saturation": "-46"
+        'saturation': '-46'
     }]
 }, {
-    "featureType": "landscape.natural",
-    "elementType": "geometry.fill",
-    "stylers": [{
-        "visibility": "on"
+    'featureType': 'landscape.natural',
+    'elementType': 'geometry.fill',
+    'stylers': [{
+        'visibility': 'on'
     }, {
-        "color": "#e0efef"
+        'color': '#e0efef'
     }]
 }, {
-    "featureType": "landscape.natural.landcover",
-    "elementType": "geometry.fill",
-    "stylers": [{
-        "saturation": "35"
+    'featureType': 'landscape.natural.landcover',
+    'elementType': 'geometry.fill',
+    'stylers': [{
+        'saturation': '35'
     }, {
-        "lightness": "8"
+        'lightness': '8'
     }, {
-        "visibility": "simplified"
+        'visibility': 'simplified'
     }]
 }, {
-    "featureType": "poi",
-    "elementType": "geometry.fill",
-    "stylers": [{
-        "visibility": "on"
+    'featureType': 'poi',
+    'elementType': 'geometry.fill',
+    'stylers': [{
+        'visibility': 'on'
     }, {
-        "hue": "#1900ff"
+        'hue': '#1900ff'
     }, {
-        "color": "#c0e8e8"
+        'color': '#c0e8e8'
     }]
 }, {
-    "featureType": "poi.attraction",
-    "elementType": "geometry.fill",
-    "stylers": [{
-        "color": "#ff0000"
+    'featureType': 'poi.attraction',
+    'elementType': 'geometry.fill',
+    'stylers': [{
+        'color': '#ff0000'
     }]
 }, {
-    "featureType": "poi.business",
-    "elementType": "geometry.fill",
-    "stylers": [{
-        "color": "#00b1ac"
+    'featureType': 'poi.business',
+    'elementType': 'geometry.fill',
+    'stylers': [{
+        'color': '#00b1ac'
     }, {
-        "lightness": "36"
+        'lightness': '36'
     }, {
-        "saturation": "-19"
+        'saturation': '-19'
     }]
 }, {
-    "featureType": "poi.business",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-        "color": "#00b1ac"
+    'featureType': 'poi.business',
+    'elementType': 'labels.text.fill',
+    'stylers': [{
+        'color': '#00b1ac'
     }, {
-        "lightness": "-30"
+        'lightness': '-30'
     }]
 }, {
-    "featureType": "poi.business",
-    "elementType": "labels.text.stroke",
-    "stylers": [{
-        "weight": "0.77"
+    'featureType': 'poi.business',
+    'elementType': 'labels.text.stroke',
+    'stylers': [{
+        'weight': '0.77'
     }, {
-        "lightness": "-45"
+        'lightness': '-45'
     }, {
-        "color": "#ffffff"
+        'color': '#ffffff'
     }]
 }, {
-    "featureType": "poi.business",
-    "elementType": "labels.icon",
-    "stylers": [{
-        "saturation": "-100"
+    'featureType': 'poi.business',
+    'elementType': 'labels.icon',
+    'stylers': [{
+        'saturation': '-100'
     }, {
-        "lightness": "0"
+        'lightness': '0'
     }]
 }, {
-    "featureType": "poi.park",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-        "color": "#006562"
+    'featureType': 'poi.park',
+    'elementType': 'labels.text.fill',
+    'stylers': [{
+        'color': '#006562'
     }, {
-        "lightness": "14"
+        'lightness': '14'
     }]
 }, {
-    "featureType": "road",
-    "elementType": "geometry",
-    "stylers": [{
-        "lightness": 100
+    'featureType': 'road',
+    'elementType': 'geometry',
+    'stylers': [{
+        'lightness': 100
     }, {
-        "visibility": "simplified"
+        'visibility': 'simplified'
     }]
 }, {
-    "featureType": "road",
-    "elementType": "labels",
-    "stylers": [{
-        "visibility": "off"
+    'featureType': 'road',
+    'elementType': 'labels',
+    'stylers': [{
+        'visibility': 'off'
     }]
 }, {
-    "featureType": "transit",
-    "elementType": "all",
-    "stylers": [{
-        "visibility": "simplified"
+    'featureType': 'transit',
+    'elementType': 'all',
+    'stylers': [{
+        'visibility': 'simplified'
     }]
 }, {
-    "featureType": "transit.line",
-    "elementType": "all",
-    "stylers": [{
-        "visibility": "off"
+    'featureType': 'transit.line',
+    'elementType': 'all',
+    'stylers': [{
+        'visibility': 'off'
     }]
 }, {
-    "featureType": "transit.line",
-    "elementType": "geometry",
-    "stylers": [{
-        "visibility": "on"
+    'featureType': 'transit.line',
+    'elementType': 'geometry',
+    'stylers': [{
+        'visibility': 'on'
     }, {
-        "lightness": 700
+        'lightness': 700
     }]
 }, {
-    "featureType": "transit.station",
-    "elementType": "all",
-    "stylers": [{
-        "visibility": "simplified"
+    'featureType': 'transit.station',
+    'elementType': 'all',
+    'stylers': [{
+        'visibility': 'simplified'
     }]
 }, {
-    "featureType": "water",
-    "elementType": "all",
-    "stylers": [{
-        "color": "#7dcdcd"
+    'featureType': 'water',
+    'elementType': 'all',
+    'stylers': [{
+        'color': '#7dcdcd'
     }]
 }, {
-    "featureType": "water",
-    "elementType": "geometry.fill",
-    "stylers": [{
-        "color": "#007e7a"
+    'featureType': 'water',
+    'elementType': 'geometry.fill',
+    'stylers': [{
+        'color': '#007e7a'
     }, {
-        "lightness": "23"
+        'lightness': '23'
     }]
 }, {
-    "featureType": "water",
-    "elementType": "geometry.stroke",
-    "stylers": [{
-        "lightness": "-6"
+    'featureType': 'water',
+    'elementType': 'geometry.stroke',
+    'stylers': [{
+        'lightness': '-6'
     }]
 }, {
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-        "color": "#007e7a"
+    'featureType': 'water',
+    'elementType': 'labels.text.fill',
+    'stylers': [{
+        'color': '#007e7a'
     }, {
-        "lightness": "22"
+        'lightness': '22'
     }]
 }, {
-    "featureType": "water",
-    "elementType": "labels.text.stroke",
-    "stylers": [{
-        "weight": "1.10"
+    'featureType': 'water',
+    'elementType': 'labels.text.stroke',
+    'stylers': [{
+        'weight': '1.10'
     }]
 }];
 
 
 function getPin(color) {
     /** @credit: https://stackoverflow.com/posts/7686977/revisions */
-    return new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + color,
+    return new google.maps.MarkerImage('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + color,
         new google.maps.Size(21, 34),
         new google.maps.Point(0, 0),
         new google.maps.Point(10, 34)
@@ -211,72 +210,105 @@ function getPin(color) {
 }
 
 function initMap() {
+    var vm = ko.dataFor(document.body);
 
     map = new google.maps.Map(document.getElementById('map'), {
-        //center: {lat: 52.516258, lng: 13.377697}, // Berlin
         center: {
             lat: 37.430594,
             lng: -122.168581
         }, // 20 Palm Drive, Stanford, CA 94305, USA
-        zoom: 12,
+        zoom: 16,
         styles: styles,
         mapTypeControl: false
     });
 
+    // Try HTML5 geolocation
+    if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            map.setCenter(pos);
+            vm.nearbySearch(vm.places, pos);
+          }, function() {
+            handleLocationError(true);
+          });
+        } else {
+            console.log("The Geolocation service failed.");
+            // Browser doesn't support Geolocation
+            handleLocationError();
+            vm.nearbySearch(vm.places);
+      }
+
+      function handleLocationError() {
+        alert('Error: The Geolocation service failed.');
+      }
+
     vm.initializeInfoWindow();
-    vm.textSearchPlaces(vm.places);
 }
 
 var Place = function(data, vm) {
-   this.name = data.name;
-   this.marker = data.marker;
-   this.icon = data.icon;
-   this.formatted_address = data.formatted_address;
-   this.marker.addListener("click", function(){
-    var marker = this;
-    //vm.infoWindow.setContent('<h4>' + marker.title + '</div>');
-    vm.name(marker.title);
-    vm.infoWindow.open(map, marker)
-   });    
+    var self = this;
 
+    this.name = data.name;
+    this.marker = data.marker;
+    this.icon = data.icon;
+    this.vicinity = data.vicinity;
+
+    this.marker.addListener('click', function(){
+        var marker = this;
+        vm.currentLocation(self);
+        vm.infoWindow.open(map, marker)
+    });    
 };
 
 var ViewModel = function() {
     var self = this;
 
-    this.places = ko.observableArray();
+    this.places = ko.observableArray([]);
 
-    this.name = ko.observable();
+    this.currentLocation = ko.observable('');
 
-    this.textSearchPlaces = function(places) {
-        var bounds = map.getBounds();
-        var placesService = new google.maps.places.PlacesService(map);
+    this.name = ko.computed(function() {
+        var location = self.currentLocation();
+        var title = location.name;
+        return title;
+    });
 
-        placesService.textSearch({
-            query: 'palo alto',
-            type: 'art_gallery',
-            bounds: bounds
-        }, function(results, status) {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
-                console.log(results);
-                results.forEach(function(result, i) {
-                    var position = result.geometry.location;
+    this.nearbySearch = function(places, position) {
+        var request = {
+            location: position,
+            radius: '500',
+            types: ['store']
+        };
+
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);
+
+        function callback(results, status) {
+            console.log(results);
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    var place = results[i];
+                    var position = place.geometry.location;
+                    var name = place.name;
                     var marker = new google.maps.Marker({
                         position: position,
-                        // icon: defaultIcon,
                         map: map,
-                        icon: getPin("d50f25"),
-                        title: result.name,
+                        icon: getPin('d50f25'),
+                        title: name,
                         animation: google.maps.Animation.Drop,
                         id: i
                     });
 
-                    result.marker = marker;
-                    places.push(new Place(result, self));
-                });
+                    place.marker = marker;
+                    places.push(new Place(place, self));
+                   
+                }
             }
-        });
-    }
+        }
+    };
 
     this.initializeInfoWindow = function() {
 
@@ -296,7 +328,7 @@ var ViewModel = function() {
          */
          google.maps.event.addListener(self.infoWindow, 'domready', function () {
             if (!isInfoWindowLoaded) {
-                ko.applyBindings(self, $("#info-window")[0]);
+                ko.applyBindings(self, $('#info-window')[0]);
                 isInfoWindowLoaded = true;
             }
         });
@@ -304,6 +336,4 @@ var ViewModel = function() {
     };
 };
 
-vm = new ViewModel();
-
-ko.applyBindings(vm);
+ko.applyBindings(new ViewModel());
